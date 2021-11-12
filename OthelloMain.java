e593be086bac4b727b704825f3e429250aa586e0
Ari <60124436+arijaye@users.noreply.github.com>
2021-11-12 12:22:53 -0500

    Create OthelloMain.java

---
+++ OthelloMain.java
@@ -0,0 +1,13 @@
+package com.company;
+
+//***************************************************
+// Filename: OthelloMain.java
+// Date: 03.17.21
+// Author(s): Ariana Sherrod-Cavanaugh
+//***************************************************
+public class OthelloMain {
+    public static void main(String args[]) {
+        new OthelloGUI();
+    }
+}
+
